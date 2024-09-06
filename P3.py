763a24fcbc67ffd8c5133edb5e46327f4cdc8ab2
Deepanshu Thakur <115526195+thedrag0nspirit@users.noreply.github.com>
2024-09-06 10:11:25 +0530

    Add files via upload

---
+++ P3.py
@@ -0,0 +1,15 @@
+#operatorts
+print(15+6)
+print(15-6)
+print(15*6)
+print(15/6)
+#exercise 1 
+#creating a calculator
+a=(8)
+b=(1)
+
+print("the value of",a,"+",b,"is:",a+b)
+print("the value of",a,"-",b,"is:",a-b)
+print("the value of",a,"*",b,"is:",a*b)
+print("the value of",a,"/",b,"is:",a/b)
+
