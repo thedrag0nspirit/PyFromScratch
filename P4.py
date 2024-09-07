0121cfd9567a51b4047fca8aade21634a540721b
Deepanshu Thakur <115526195+thedrag0nspirit@users.noreply.github.com>
2024-09-07 23:26:51 +0530

    Add files via upload

---
+++ P4.py
@@ -0,0 +1,18 @@
+# #typecasting
+a=1
+b=2
+print(a+b)
+
+# # implicit typecasting
+c=1
+d=1.9
+print(c+d)
+
+# # program to tAKE INPUT 
+a=input("enter your name:")
+print ("My name is =")
+
+# 2
+x=input("enter number 1:") #basically here we have ton declare the following integer.after that they will be added
+y=input("enter number 2:")
+print (int(x)+int(y))
